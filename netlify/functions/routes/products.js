0ed7515a54354c0e065218058179f02bc504b733
Muhammad Yasin <yaseenbalich71@gmail.com>
2026-07-19 19:11:48 +0500

    Add auth middleware to product routes for admin-only CRUD

--- netlify/functions/routes/products.js
+++ netlify/functions/routes/products.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const { protect, isAdmin } = require('../middleware/auth');
 const { getProducts, getProductById, createProduct, updateProduct, deleteProduct } = require('../controllers/productController');
 
 router.get('/', getProducts);
 router.get('/:id', getProductById);
-router.post('/', createProduct);
-router.put('/:id', updateProduct);
-router.delete('/:id', deleteProduct);
+router.post('/', protect, isAdmin, createProduct);
+router.put('/:id', protect, isAdmin, updateProduct);
+router.delete('/:id', protect, isAdmin, deleteProduct);
 
 module.exports = router;
